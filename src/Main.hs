af110a1c3b8155bb799f79788e6f6fdc2e802877
KolodeznyDiver <KolodeznyDiver@users.noreply.github.com>
2016-05-14 03:08:55 +0400

    version:             0.1.0.1

--- src/Main.hs
+++ src/Main.hs
@@ -1,56 +1,80 @@
+{-# LANGUAGE OverloadedStrings,PatternSynonyms,ScopedTypeVariables #-}
 module Main where
 
-import System.IO
-import Network.HTTP
-import Pipes
-import Pipes.HTTP
-import qualified Pipes.ByteString as PB
-import Text.Parsec
-import Data.Char(toLower,toUpper)
-import Control.Monad
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Client        (defaultManagerSettings, newManager)
+import           Network.HTTP.Simple
+import qualified Data.Text as T
+import Data.Text.Read
+import Text.XML
+import Control.Monad (forM_)
+import Text.XML.Lens
+import System.EasyFile
+import Options.Applicative
 
-type ParserFunType = Parsec String ()
+pattern SiteURL = ("http://hackage.haskell.org") :: String
+pattern TopPage = ("/packages/top")  :: String
+pattern PackagesPrefix = ("/package/")  :: String
+pattern LocalOutDir = ("package")  :: String
 
-caseInsensitiveChar:: Char -> ParserFunType ()
-caseInsensitiveChar c = void $ char (toLower c) <|> char (toUpper c)
-
-caseInsensitiveString:: String -> ParserFunType ()
-caseInsensitiveString s = try (mapM_ caseInsensitiveChar s)
-
-download:: Manager -> String -> IO ()
-download m n = do 
-    req <- parseUrl ("http://hackage.haskell.org/package/"++n)
-    withHTTP req m $ \resp ->
-        withBinaryFile (concat["package/",n,".html"]) WriteMode $ \h ->
-                runEffect $ responseBody resp >-> PB.toHandle h
+data Cfg = Cfg { dwnldPerMonthLimit :: Int
+               }
+            
+cfgParser:: Parser Cfg
+cfgParser = Cfg <$>
+    option auto (
+        value 0 <>
+        long "downloadsLimit" <>
+        short 'd' <>
+        metavar "Number" <>
+        help "Minimal downloads per month for load" )
 
 main :: IO ()
-main = do
+main = execParser opts >>= main'
+  where
+    opts = info (helper <*> cfgParser)
+      ( -- fullDesc <>
+      progDesc "Download html package desciptions from hackage.haskell.org"
+      <> header "HackageStat v0.1.0.1- Hackage package description downloader" )
+
+main' :: Cfg -> IO ()
+main' (Cfg dwnldsLim) = do
+    let topPg = SiteURL ++ TopPage
+    putStrLn $ concat ["Downloading ", topPg, " ..."]
     m <- newManager defaultManagerSettings
-    a <- simpleHTTP (getRequest "http://hackage.haskell.org/packages/top")
-    case a of
-        (Left err) -> print err
-        (Right resp) -> let body = rspBody resp in
-                        case parse (parsePkgNames []) "" body of
-                            Left perr  -> print perr
-                            Right xs  -> 
-                                let sLn = concat [" from ", show $ length xs,
-                                            " : "] in
-                                forM_ (zip xs [1 :: Int ..]) $ \(n,i) -> do
-                                        putStrLn $ concat [ "downloading ",
-                                            show i,sLn,n]
-                                        download m n 
-                                          
-  where parsePkgNames l = do
-            skipMany $ noneOf "<"
-            r <- optionMaybe lnk
-            case r of 
-                Just n -> parsePkgNames $ n:l
-                _ -> do
-                    e <- optionMaybe anyToken
-                    case e of
-                      Nothing -> return l
-                      _ -> parsePkgNames l
-        lnk = do
-            caseInsensitiveString "<a href=\"/package/"
-            many1 (noneOf "\"")
+    request <- parseRequest $ topPg
+    response <- httpLBS $ setRequestManager m request
+    putStrLn $ concat ["Parsing ", topPg, " ..."]
+    case parseLBS def{psDecodeEntities = decodeHtmlEntities} $ getResponseBody response of
+        Left uerr  -> print uerr
+        Right doc -> do
+            let hel n = el Name{nameLocalName = n, 
+                           nameNamespace = Just "http://www.w3.org/1999/xhtml",
+                           namePrefix = Nothing}
+                rows = doc ^.. root ./ hel "body" ./ attributeIs "id" "content"
+                        ./ entire ./ hel "tr" 
+                byDwnlds i [] = i
+                byDwnlds i (s:ss) = case decimal s of
+                                        Right (d,_) 
+                                            | d < dwnldsLim -> i
+                                            | otherwise -> byDwnlds (i+1) ss
+                                        _ -> i
+                dwnlds = rows ^.. traverse ./ hel "td" . text
+                allPkgs = rows ^.. traverse ./ hel "td" ./ hel "a" . text
+                pkgs = map T.unpack $ take (byDwnlds 0 dwnlds) allPkgs
+                cAllPk = show $ length allPkgs
+                cPk = show $ length pkgs
+            putStrLn $ concat ["A total of ", cAllPk, 
+               " packets of them fulfill the criteria ",cPk]
+            let sLn = concat [" from ", cPk," : "] 
+            createDirectoryIfMissing False outDir
+            forM_ (zip pkgs [1 :: Int ..]) $ \(n,i) -> do
+                putStrLn $ concat [ "downloading ",show i,sLn,n]
+                download m n 
+            putStrLn "Loading completed"
+  where download m n = do
+            request <- parseRequest $ concat[SiteURL,PackagesPrefix,n]
+            response <- httpLBS $ setRequestManager m request
+            L8.writeFile (outDir </> n System.EasyFile.<.> "html") $
+                getResponseBody response
+        outDir = normalise $ joinPath [".",LocalOutDir]
